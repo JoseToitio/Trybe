f9423e5ace024d902c6d1e3ab91da84665eb2f21
Jose <josetoitio@gmail.com>
2021-09-10 15:44:45 -0300

    exercicios concluidos

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    let changeColor = document.querySelector('#select1')
+    let changeColor = document.querySelector('#select1');
     changeColor.addEventListener('change', function () {
         let selected = changeColor.selectedOptions[0];
         document.body.style.backgroundColor = selected.value;
@@ -12,18 +12,33 @@ window.onload = function () {
         localStorage.setItem('color-text', selected.value );
     })
 
+    let changeFontFamily = document.querySelector('#select3');
+    changeFontFamily.addEventListener('change', function (){
+        let fonts = changeFontFamily.selectedOptions[0];
+        document.body.style.fontFamily = fonts.value;
+        localStorage.setItem('font-family', fonts.value)
+    })
+
     let inputFontSize = document.querySelector('input[name= "font-size"]')
     inputFontSize.addEventListener('change', function () {
        let p = document.querySelector('#content-text');
        p.style.fontSize = `${inputFontSize.value}px`
-
-       localStorage.setItem('font-size', `${inputFontSize.value}px` )
-       
+       localStorage.setItem('font-size', `${inputFontSize.value}px` ) 
+    })
+    let inputLineHeight = document.querySelector('input[name = "line-height"]')
+    inputLineHeight.addEventListener('change', function () {
+        let p = document.querySelector('#content-text');
+        p.style.lineHeight = `${inputLineHeight.value}px`
+        localStorage.setItem('line-height', `${inputLineHeight.value}px`);
     })
     function savedAllStorage () {
         let savedBackGround = localStorage.getItem('background-color');
         let savedColorText = localStorage.getItem('color-text');
         let savedFontSize = localStorage.getItem('font-size');
+        let savedFontFamily = localStorage.getItem('font-family');
+        let savedLineHeight = localStorage.getItem('line-height');
+        document.querySelector('#content-text').style.lineHeight = savedLineHeight;
+        document.body.style.fontFamily = savedFontFamily;
         document.querySelector('#content-text').style.fontSize = savedFontSize;
         document.body.style.color = savedColorText
         document.body.style.backgroundColor = savedBackGround;
