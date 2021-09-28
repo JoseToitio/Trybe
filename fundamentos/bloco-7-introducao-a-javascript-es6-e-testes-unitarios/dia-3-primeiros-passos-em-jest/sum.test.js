bc51ca807a0a10e8b53ce7ec744626953a7f8781
Jose <josetoitio@gmail.com>
2021-09-28 20:31:14 -0300

    exercicio 3 completo

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,4 +1,4 @@
-const {sum, myRemove} = require('./sum');
+const {sum, myRemove, myFizzBuzz} = require('./sum');
 
 describe('The function sum()', () => {
   it('Recebe o valor 4 e 5 e retorna 9', () => {
@@ -29,5 +29,27 @@ describe('The function myRemove()', () => {
 
   it('recebe myRemove([1, 2, 3, 4], 5) e retorna [1,2,3,4] ', () => {
     expect(myRemove([1,2,3,4],5)).toStrictEqual([1, 2, 3, 4]);
+  });
+});
+
+describe('The function myFizzBuzz()', () => {
+  it('recebe o valor 15 e retorna: fizzbuzz', () => {
+    expect(myFizzBuzz(15)).toBe('fizzbuzz');
+  });
+
+  it('recebe o valor 6 e retorna: fizz', () => {
+    expect(myFizzBuzz(6)).toBe('fizz');
+  });
+
+  it('recebe o valor 10 e retorna: buzz', () => {
+    expect(myFizzBuzz(10)).toBe('buzz');
+  });
+
+  it('recebe o valor 2 e retorna: 2', () => {
+    expect(myFizzBuzz(2)).toBe(2);
+  });
+
+  it('recebe o valor "quatro" e retorna: fizzbuzz', () => {
+    expect(myFizzBuzz('quatro')).toBe(false);
   });
 });
