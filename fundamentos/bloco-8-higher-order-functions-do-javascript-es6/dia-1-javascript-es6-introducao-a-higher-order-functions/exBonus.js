7d227c83055e77f72774a6a6652f7baaf0e1ea0b
Jose <josetoitio@gmail.com>
2021-10-04 20:33:14 -0300

    exercicio bonus parte 1 resolvido

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exBonus.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exBonus.js
@@ -19,3 +19,38 @@ const dragon = {
 };
 
 const battleMembers = { mage, warrior, dragon };
+
+const dragonDamage = (dragon) => {
+  const { strength } = dragon
+  const minDamage = 15;
+  const damage = Math.floor(Math.random() * (strength - minDamage + 1)) + minDamage; 
+  return damage;
+}
+
+const warriorDamage = (warrior) => {
+  const { strength } = warrior;
+  const { weaponDmg } = warrior;
+  const maxDamage = strength * weaponDmg;
+  const minDamage = strength;
+  const damage = Math.floor(Math.random() * (maxDamage - minDamage + 1)) + minDamage;
+  return damage;
+}
+
+const mageDamage = (mage) => {
+  const mageMana = mage.mana  // 125
+  const { intelligence } = mage;  //45
+  const minDamage = intelligence;
+  const maxDamage = intelligence * 2;
+  const damage = Math.floor(Math.random() * (maxDamage - minDamage + 1) + minDamage);
+  const status = {
+    dano: 'Não possui mana suficiente',
+    mana: 0,
+  }
+  if (mageMana > 15) {
+    status.mana = 15;
+    status.dano = damage;
+    return status;
+  }
+  return status;
+}
+console.log(mageDamage(mage))
