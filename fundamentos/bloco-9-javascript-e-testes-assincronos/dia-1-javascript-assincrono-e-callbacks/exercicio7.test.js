172749cb3418f2ca3f38841500971f1bc95e894c
Jose <josetoitio@gmail.com>
2021-10-15 23:06:55 -0300

    exercicio 7 completo

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicio7.test.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicio7.test.js
@@ -1,5 +1,18 @@
+const { it, expect } = require("@jest/globals");
+
 const uppercase = (str, callback) => {
   setTimeout(() => {
     callback(str.toUpperCase());
   }, 500);
 };
+
+it('Testando se transforma as letras de uma palavra em letras maiúsculas.', (done) => {
+  uppercase('Baile Do Mandela', (result) => {
+    try {
+      expect(result).toBe('BAILE DO MANDELA')
+      done();
+    } catch (error) {
+      done(error);
+    }
+  })
+});
