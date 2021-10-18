064a27670e96eea2af468adc3effa501e4aa8c07
Jose <josetoitio@gmail.com>
2021-10-18 20:10:50 -0300

    exercicio bonus feito

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/exercicio2/api.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/exercicio2/api.js
@@ -1,29 +1,45 @@
+// api.js
+const fetchCoins = async () => {
+  const url = 'https://api.coincap.io/v2/assets';
 
-async function fetchCriptos () {
-  const coins = await fetch(`https://api.coincap.io/v2/assets`)
-  .then(response => response.json())
-  .then(data => {
-    return data.data
-  })
+  const coins = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.data)
+    .catch((error) => error.toString());
 
   return coins;
-}
+};
 
-async function setCoins () {
-  const coins = await fetchCriptos();
+const fetchUsdCurrencies = async () => {
+  const baseUrl = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest'
+  const usdEndpoint = '/currencies/usd.min.json'
+  const url = baseUrl.concat(usdEndpoint);
 
-  const coinsList = document.querySelector('#criptos');
+  const usdCurrencies = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.usd)
+    .catch((error) => error.toString());
+
+  return usdCurrencies;
+};
+
+const setCoins = async () => {
+  const coins = await fetchCoins();
+  const usdCurrencies = await fetchUsdCurrencies();
+
+  const coinsList = document.getElementById('coins-list');
 
   coins
-  .filter((coin) => Number(coin.rank) <= 10)
-  .forEach((coin) => {
-    const newLi = document.createElement('li');
-    const usdPrice = Number(coin.priceUsd);
-    newLi.innerText = `${coin.name} (${coin.symbol}): ${usdPrice.toFixed(2)}`;
+    .filter((coin) => Number(coin.rank) <= 10)
+    .forEach((coin) => {
+      const newLi = document.createElement('li');
+      const usdPrice = Number(coin.priceUsd);
+      const brlPrice = usdPrice * usdCurrencies.brl;
 
-    coinsList.appendChild(newLi);
-  });
-}
+      newLi.innerText = `${coin.name} (${coin.symbol}): ${brlPrice.toFixed(2)}`;
 
+      coinsList.appendChild(newLi);
+    });
+};
 
-window.onload =  () => setCoins();
+window.onload = () => setCoins();
