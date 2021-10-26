3b92f51d3fe1942035e037eb12976f21336f5965
Jose <josetoitio@gmail.com>
2021-10-26 15:20:38 -0300

    exercicio 1 completo

--- Front End/bloco-10-introducao-a-react/dia-1-hello-world-no-react/dia-1-hello-world/src/App.js
+++ Front End/bloco-10-introducao-a-react/dia-1-hello-world-no-react/dia-1-hello-world/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+
+const taskList = ['jogar bola', 'correr', 'fazer academia', 'academia de luta', 'estudar react', 'estudar logica de programação'];
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+};
+
+const allTaks = () => {
+  return taskList.map((element) => Task(element));
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {allTaks()}
+      </ul>
     </div>
   );
 }
