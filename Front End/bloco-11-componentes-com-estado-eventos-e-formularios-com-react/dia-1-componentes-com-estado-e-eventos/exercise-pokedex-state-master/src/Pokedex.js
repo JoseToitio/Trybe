64218f1b91f63c6676a331cf09dc3465dc2275ba
Jose <josetoitio@gmail.com>
2021-11-03 19:26:55 -0300

    adiciona button

--- Front End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercise-pokedex-state-master/src/Pokedex.js
+++ Front End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercise-pokedex-state-master/src/Pokedex.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import Pokemon from './Pokemon';
+import React from "react";
+import Pokemon from "./Pokemon";
+import Button from "./Button";
 
 class Pokedex extends React.Component {
-    render() {
-        return (
-            <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemonIndex: 0,
+      filteredType: "all",
+    };
+  }
+  render() {
+    return (
+      <div className="pokedex">
+        {this.props.pokemons.map((pokemon) => (
+          <Pokemon key={pokemon.id} pokemon={pokemon} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Pokedex;
