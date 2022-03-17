665309a9dab8bf73b35fff92bffc4947c3efb329
Toitio <josetoitio@gmail.com>
2022-03-17 01:31:01 -0300

    ex 2 completo

--- Back End/bloco-22/dia-4/exercises-26-4/index.js
+++ Back End/bloco-22/dia-4/exercises-26-4/index.js
@@ -6,5 +6,8 @@ app.use(bodyParser.json());
 app.get('/ping', (req, res) => {
   res.json({ "message": "pong"})
 });
-
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(200).json({"message": `Hello, ${name}!`})
+})
 app.listen(3001, () => console.log('porta 3001'));
