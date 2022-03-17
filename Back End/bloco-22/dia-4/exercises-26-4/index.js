c6a0dd2b4e7c11b8022cf84d04e5a0b430e1adec
Toitio <josetoitio@gmail.com>
2022-03-17 01:35:45 -0300

    ex 3 completo

--- Back End/bloco-22/dia-4/exercises-26-4/index.js
+++ Back End/bloco-22/dia-4/exercises-26-4/index.js
@@ -9,5 +9,10 @@ app.get('/ping', (req, res) => {
 app.post('/hello', (req, res) => {
   const { name } = req.body;
   res.status(200).json({"message": `Hello, ${name}!`})
+});
+app.post('/greetings', (req, res) => {
+  const {name, age} = req.body;
+  if(parseInt(age, 10) <= 17) return res.status(401).json({message: "Unauthorized"});
+  return res.status(200).json({message: `Hello, ${name}!`})
 })
 app.listen(3001, () => console.log('porta 3001'));
